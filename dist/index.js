2d3a70b42ef8076e3a7ced4b03744d7e89004357
nealfwilliams <nealfwilliams@gmail.com>
2017-10-04 06:31:10 -0700

    initial

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+export const createActionAppliers = (actionCreators, reducer) => {
+  return _.mapValues(actionCreators, actionCreator => (state, ...actionCreatorArgs) => reducer(state, actionCreator(...actionCreatorArgs)));
+};
+
+export class ChainedActionApplier {
+  constructor(actionCreators, reducer) {
+    this.actionAppliers = createActionAppliers(actionCreators, reducer);
+  }
+
+  applyActions(state, ...actionCreators) {
+    let generatedStates = [];
+    let nthState = state;
+    for (let actionCreator of actionCreators) {
+      let name = actionCreator.name;
+      let args = actionCreator.args || [];
+      if (!(name in this.actionAppliers)) {
+        throw `Action Creator: ${name} not found`;
+      } else {
+        nthState = this.actionAppliers[name](nthState, ...args);
+        generatedStates.push(nthState);
+      }
+    }
+    return _.reverse(generatedStates);
+  }
+}
